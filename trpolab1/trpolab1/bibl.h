ed5c8bd052fb71f9291df2389307424bfb2bfd9d
SamAlexander <zvinnik@me.com>
2016-04-08 20:25:34 +0600

    решение квадратных уравнений

---
+++ trpolab1/trpolab1/bibl.h
@@ -0,0 +1,14 @@
+//
+//  bibl.h
+//  trpolab1
+//
+//  Created by Александр Эм on 19.03.16.
+//  Copyright © 2016 Александр Эм. All rights reserved.
+//
+
+#ifndef bibl_h
+#define bibl_h
+
+#include <stdio.h>
+
+#endif /* bibl_h */
